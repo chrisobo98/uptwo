3da132a0ef043dd5f3a1648917ddf3a23c4409da
chrisobo98 <christopherbermudez98@gmail.com>
2025-03-03 15:47:13 -0500

    feat: uninstalled nuxt google fonts bc still results in external requests, which slows down the initial load

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,26 +16,6 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxtjs/seo',
     '@nuxt/content',
-    [
-      '@nuxtjs/google-fonts',
-      {
-        families: {
-          Cinzel: {
-            wght: [400, 700], // Regular and Bold for Cinzel
-          },
-          Lora: {
-            wght: [400, 700], // Regular and Bold for Lora
-            ital: [400, 700], // Italic variants
-          },
-          Montserrat: {
-            wght: [100, 300, 400, 700], // Thin, Light, Regular, and Bold for Montserrat
-          },
-        },
-        display: 'swap', // Ensure fonts are loaded with a fallback
-        subsets: 'latin', // Optimize for Latin-based languages
-        preload: true, // Preload fonts for faster rendering
-      },
-    ],
     'nuxt-viewport',
     '@nuxtjs/cloudinary',
     '@nuxt/icon',
