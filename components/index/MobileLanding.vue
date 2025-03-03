92eaf49b1468e57480a1635fd0d31c3c276c41f6
chrisobo98 <christopherbermudez98@gmail.com>
2025-03-03 13:14:17 -0500

    feat: Reduce Redundant Calls & Improve Caching on mobile landing

--- components/index/MobileLanding.vue
+++ components/index/MobileLanding.vue
@@ -2,34 +2,33 @@
   <section class="hero-section">
     <!-- Video Background -->
     <div class="hero-video opacity-40">
-      <video autoplay loop muted playsinline>
-        <source src="https://cdn.prod.website-files.com/66b1e669bf6b613debec2857/66c059230ce17596b9a93ac0_Hero_Video-comp-transcode.mp4" type="video/mp4" />
-        <source src="https://cdn.prod.website-files.com/66b1e669bf6b613debec2857/66c059230ce17596b9a93ac0_Hero_Video-comp-transcode.webm" type="video/webm" />
-        Your browser does not support the video tag.
+      <video autoplay loop muted playsinline preload="auto">
+        <source
+          src="https://cdn.prod.website-files.com/66b1e669bf6b613debec2857/66c059230ce17596b9a93ac0_Hero_Video-comp-transcode.mp4"
+          type="video/mp4"
+        />
       </video>
     </div>
-
-
   </section>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted } from "vue";
 import gsap from "gsap";
 
 onMounted(() => {
   // Add GSAP animations for text lines
-  const textLines = document.querySelectorAll('.hero-text h1 span')
+  const textLines = document.querySelectorAll(".hero-text h1 span");
   textLines.forEach((line, index) => {
     gsap.from(line, {
       opacity: 0,
       y: 20,
       duration: 0.8,
-      delay: parseFloat(line.style.getPropertyValue('--delay')),
-      ease: 'power3.out',
-    })
-  })
-})
+      delay: parseFloat(line.style.getPropertyValue("--delay")),
+      ease: "power3.out",
+    });
+  });
+});
 </script>
 
 <style scoped>
