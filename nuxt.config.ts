b10893e76d88856fc264e27b2761c82922431020
chrisobo98 <christopherbermudez98@gmail.com>
2025-03-03 16:06:20 -0500

    feat: added nuxt fonts back in not the performance boost we were looking for

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,29 +7,37 @@ export default defineNuxtConfig({
       routes: ['/'], // Ensures homepage is always prerendered
     },
   },
-  ogImage: { enabled: false },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-swiper',
-    'v-gsap-nuxt',
-    '@nuxt/image',
-    '@nuxtjs/seo',
-    '@nuxt/content',
-    'nuxt-viewport',
-    '@nuxtjs/cloudinary',
-    '@nuxt/icon',
-  ],
-  robots: {
-    allow: '/', // Allows all pages to be crawled
-    sitemap: '/sitemap.xml' // Optional, if you have a sitemap
-  },
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-swiper', 'v-gsap-nuxt', '@nuxt/image', '@nuxtjs/seo', '@nuxt/content', [
+    '@nuxtjs/google-fonts',
+    {
+      families: {
+        Cinzel: {
+          wght: [400, 700], // Regular and Bold for Cinzel
+        },
+        Lora: {
+          wght: [400, 700], // Regular and Bold for Lora
+          ital: [400, 700], // Italic variants
+        },
+        Montserrat: {
+          wght: [100, 300, 400, 700], // Thin, Light, Regular, and Bold for Montserrat
+        },
+      },
+      display: 'swap', // Ensure fonts are loaded with a fallback
+      subsets: 'latin', // Optimize for Latin-based languages
+      preload: true, // Preload fonts for faster rendering
+    },
+  ], 'nuxt-viewport', '@nuxtjs/cloudinary', '@nuxt/icon', '@nuxtjs/google-fonts'],
   image: {
     provider: 'cloudinary',
     cloudinary: {
       baseURL: 'https://res.cloudinary.com/dkgpny3ca/image/upload/', // Replace with your Cloudinary base URL
     },
+  },
+  robots: {
+    allow: '/', // Allows all pages to be crawled
+    sitemap: '/sitemap.xml' // Optional, if you have a sitemap
   },
   css: ['~/assets/css/main.css'],
 })
